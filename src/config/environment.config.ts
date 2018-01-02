c8a87631f36c7ae658ffdc2739e9fcc944d21378
Victor Novy <novyvictor@gmail.com>
2018-01-02 21:51:16 -0200

    Add config environment

---
+++ src/config/environment.config.ts
@@ -0,0 +1,4 @@
+export const environmentConfig = {
+    isProd: false,
+    dbName: 'batponto-test'
+}
--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,3 +1,5 @@
 import { firebase } from './firebase';
+import { environmentConfig } from './environment.config';
 
 export const firebaseConfig = firebase;
+export const environment = environmentConfig;
